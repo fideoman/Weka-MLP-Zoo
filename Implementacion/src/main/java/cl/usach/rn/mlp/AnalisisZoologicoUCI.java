5e8bfacfaedb4d568e58a8f327e8fdcc4abff864
fideoman <idsl1012@gmail.com>
2018-07-08 08:58:30 -0400

    Isaac: Limpieza, comentarios, parámetros básicos.

--- Implementacion/src/main/java/cl/usach/rn/mlp/AnalisisZoologicoUCI.java
+++ Implementacion/src/main/java/cl/usach/rn/mlp/AnalisisZoologicoUCI.java
@@ -29,13 +29,32 @@ public class AnalisisZoologicoUCI {
 			MultilayerPerceptron mlp = new MultilayerPerceptron();
 
 			// Parámetros:
-			mlp.setLearningRate(0.1);
-			mlp.setMomentum(0.1);
-			mlp.setTrainingTime(20000);
-			mlp.setSeed(0);
-			mlp.setValidationSetSize(0);
-			mlp.setValidationThreshold(20);
-			mlp.setHiddenLayers("4");
+			mlp.setLearningRate(0.1); 				// Tasa de aprendizaje. De 0 a 1. Por defecto: 0.3
+			mlp.setMomentum(0.1); 					// Momentum para el algoritmo de retro-propagación. De 0 a 1. Por defecto: 0.2
+			mlp.setTrainingTime(8000); 				// Tiempo de entrenamiento, en épocas. Por defecto: 500
+			mlp.setValidationSetSize(0); 			/* Tamaño (en porcentaje) del grupo de validación 
+														usado para terminar el entrenamiento. 
+														Si no es cero, puede pre-calcular número de épocas. 
+														De 0 a 100. Por defecto: 0 */
+			mlp.setSeed(0); 						// Semilla. El valor para alimentar al generator de números aleatorios. Por defecto: 0
+			mlp.setValidationThreshold(20); 		/* El número máximo de errores consecutivos permitido para las pruebas de validación
+														antes que la red termine. Valor debe ser mayor que cero. Por defecto: 20*/
+			mlp.setGUI(false); 						// Para abrir, o no, una guía gráfica.
+			mlp.setAutoBuild(true); 				// En el caso de elegir GUI, ésta opción indica si creamos el grafo o no
+			mlp.setNominalToBinaryFilter(true); 	// Para pasar variables nominales a binarias, de manera automática.
+			mlp.setHiddenLayers("4"); 				/* Las capas ocultas creadas para la red.
+			  											El valor debería ser una lista de números naturales separada por comas,
+					  									o las letras 'a' = (atributos + clases) / 2, 
+					  									'i' = atributos, 
+					  									'o' = clases, 
+					  									't' = atributos .+ clases (para comodines).
+					  									Por defecto = a */
+			mlp.setNormalizeNumericClass(true); 	// Indicar acá si las clases numéricas serán normalizadas o no.
+			mlp.setNormalizeAttributes(true); 		// Indicar acá si los atributos serán normalizados o no.
+			mlp.setReset(true); 					// Indicar acá si se permite que la red sea reiniciada o no.
+			mlp.setDecay(false); 					// Indicar acá si se permite que la tasa de aprendizaje pueda decrecer o no.
+			mlp.setDoNotCheckCapabilities(false); 	// Indicar acá si se permite que la librería pueda analizar las capacidades de la red o no.
+			mlp.setDebug(false); 					// Indicar acá si la ejecución es depurable o no.
 
 			// Se construye el clasificador
 			mlp.buildClassifier(entrenamiento);
