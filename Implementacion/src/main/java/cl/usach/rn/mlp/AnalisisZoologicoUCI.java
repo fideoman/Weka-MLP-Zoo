d4795eec8d1632cb1dcd3abaed401f962a1110e8
fideoman <idsl1012@gmail.com>
2018-07-08 13:28:29 -0400

    Isaac: Mejoras varias.

--- Implementacion/src/main/java/cl/usach/rn/mlp/AnalisisZoologicoUCI.java
+++ Implementacion/src/main/java/cl/usach/rn/mlp/AnalisisZoologicoUCI.java
@@ -2,11 +2,14 @@ package cl.usach.rn.mlp;
 
 import java.io.File;
 import java.io.FileReader;
+import java.util.Arrays;
 
+import cl.usach.rn.mlp.utilidades.IWSS;
 import weka.classifiers.Evaluation;
 import weka.classifiers.functions.MultilayerPerceptron;
 import weka.core.Instances;
 
+@SuppressWarnings("unused")
 public class AnalisisZoologicoUCI {
 
 	public static void main(String[] args) {
@@ -14,24 +17,40 @@ public class AnalisisZoologicoUCI {
 
 		try {
 			// Lectura del set de entrenamiento y prueba. Se cierran los archivos inmediatamente, una vez leídas las instancias.
-			FileReader lectorEntrenamiento = new FileReader(new File(cargadorContexto.getResource("zoo.arff").toURI()));
-			FileReader lectorPrueba = new FileReader(new File(cargadorContexto.getResource("zoo.arff").toURI()));
+			FileReader lectorEntrenamiento = new FileReader(new File(cargadorContexto.getResource("zoo-5_atributos_eliminados.arff").toURI()));
+			FileReader lectorPrueba = new FileReader(new File(cargadorContexto.getResource("zoo-5_atributos_eliminados.arff").toURI()));
 			Instances entrenamiento = new Instances(lectorEntrenamiento);
 			Instances prueba = new Instances(lectorPrueba);
 			lectorEntrenamiento.close();
 			lectorPrueba.close();
 			
-			// Establecemos la columna que tiene la clase
+			// Establecemos la columna que tiene la clase. Ese es el "target".
 			entrenamiento.setClassIndex(entrenamiento.numAttributes() - 1);
 			prueba.setClassIndex(prueba.numAttributes() - 1);
 
-			// Perceptrón multicapa.
+			// Clasificador: Perceptrón multicapa.
 			MultilayerPerceptron mlp = new MultilayerPerceptron();
+			
+			// Rutina de ranking de atributos para eliminación.
+			/* Implementado por Pablo Bermejo, 
+			 * Jose A. Gomez and Jose,(2011). Improving
+			 * Incremental Wrapper-Based Subset Selection via Replacement and Early
+			 * Stopping. International Journal of Pattern Recognition and Artificial
+			 * Intelligence. 25(5):605-625 */
+			
+			// Entrega, por cada atributo, su nivel de "eliminable".
+			// IWSS iwss = new IWSS();
+			// System.out.println(Arrays.toString(iwss.getRanking(prueba)));
+			// Resultado del Zoo Dataset original:
+			// [0, 4, 3, 8, 1, 2, 9, 10, 14, 13, 19, 15, 12, 17, 5, 6, 21, 11, 18, 7, 16, 20]
+			
+			// En concreto, los 5 más eliminables son:
+			// 5_patas: 21, pequeño: 20, respirador: 19, 8_patas: 18, 0_patas: 17
 
 			// Parámetros:
 			mlp.setLearningRate(0.1); 				// Tasa de aprendizaje. De 0 a 1. Por defecto: 0.3
 			mlp.setMomentum(0.1); 					// Momentum para el algoritmo de retro-propagación. De 0 a 1. Por defecto: 0.2
-			mlp.setTrainingTime(8000); 				// Tiempo de entrenamiento, en épocas. Por defecto: 500
+			mlp.setTrainingTime(5000); 				// Tiempo de entrenamiento, en épocas. Por defecto: 500
 			mlp.setValidationSetSize(0); 			/* Tamaño (en porcentaje) del grupo de validación 
 														usado para terminar el entrenamiento. 
 														Si no es cero, puede pre-calcular número de épocas. 
@@ -40,7 +59,7 @@ public class AnalisisZoologicoUCI {
 			mlp.setValidationThreshold(20); 		/* El número máximo de errores consecutivos permitido para las pruebas de validación
 														antes que la red termine. Valor debe ser mayor que cero. Por defecto: 20*/
 			mlp.setGUI(false); 						// Para abrir, o no, una guía gráfica.
-			mlp.setAutoBuild(true); 				// En el caso de elegir GUI, ésta opción indica si creamos el grafo o no
+			mlp.setAutoBuild(false); 				// En el caso de elegir GUI, ésta opción indica si creamos el grafo o no
 			mlp.setNominalToBinaryFilter(true); 	// Para pasar variables nominales a binarias, de manera automática.
 			mlp.setHiddenLayers("4"); 				/* Las capas ocultas creadas para la red.
 			  											El valor debería ser una lista de números naturales separada por comas,
@@ -63,10 +82,27 @@ public class AnalisisZoologicoUCI {
 			Evaluation eval = new Evaluation(entrenamiento);
 			eval.evaluateModel(mlp, prueba);
 			
-			// Imprimir todos los resultados.
-			System.out.println(eval.toSummaryString());
-			System.out.println(eval.toMatrixString());
-			System.out.println(mlp.toString());
+			// Imprimir todos los resultados.			
+			System.out.println("Error medio absoluto: " + eval.meanAbsoluteError());
+			System.out.println("Error medio cuadrado: " + eval.rootMeanSquaredError());
+			System.out.println("Error relativo absoluto: " + eval.relativeAbsoluteError()+" %");
+			System.out.println("Error relativo cuadrado: " + eval.rootRelativeSquaredError()+" %");
+			System.out.println("Número de instancias: " + eval.numInstances());
+			System.out.println("Costo de las clasificaciones incorrectas sobre el total (promedio): " + eval.avgCost());
+			System.out.println("Costo total SUM (costo de cada predicción x ancho de cada instancia) : " + eval.totalCost());
+			System.out.println("Instancias correctamente clasificadas (o suma de sus pesos): " + eval.correct());
+			System.out.println("Porcentaje correcto: " + eval.pctCorrect());
+			System.out.println("Instancias incorrectamente clasificadas (o suma de sus pesos): " + eval.incorrect());
+			System.out.println("Porcentaje incorrecto: " + eval.pctIncorrect());
+			System.out.println("Instancias no clasificadas (o suma de sus pesos): " + eval.unclassified());
+			System.out.println("Porcentaje no clasificado: " + eval.pctUnclassified());
+			System.out.println("Tasa de error: " + eval.errorRate());
+			System.out.println("Estadística Kappa (concordancia): " + eval.kappa());
+			System.out.println("Puntaje medio Kononenko & Bratko: " + eval.KBMeanInformation());
+			System.out.println("Entropía total del esquema: " + eval.SFSchemeEntropy());
+			System.out.println("Tamaño promedio de las regiones predecidas: " + eval.sizeOfPredictedRegions());
+			System.out.println(eval.toClassDetailsString("Detalle de clases:"));
+			System.out.println(eval.toMatrixString("Matriz de confusión:"));
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
